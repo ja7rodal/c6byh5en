45685e22d275256dbb49c9262b4f4aad4e7830a2
Jasson Rdz <ja7rodal@gmail.com>
2017-01-11 20:05:09 -0500

    fix render destroy

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -29,7 +29,7 @@ class Api::V1::ProductsController < ApplicationController
 	def destroy
 		product = Product.find(params[:id])
 		if product.destroy
-			render status:204
+			render status:204, nothing: true
 		end
 	end
 
@@ -39,3 +39,19 @@ class Api::V1::ProductsController < ApplicationController
 	end
 
 end
+
+
+#  def update
+#    product = Product.find(params[:id])
+#    if product.update(product_params)
+#      render json: product, status: 200
+#    else
+#      render json: { errors: product.errors }, status: 422
+#    end
+#  end
+#  
+#  def destroy
+#    product = Product.find(params[:id])
+#    product.destroy
+#    head 204
+#  end
