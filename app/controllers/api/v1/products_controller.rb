d3232fda0714c08ae68acf4fdcd387c8a0f5e8eb
Jasson Rdz <ja7rodal@gmail.com>
2017-01-11 19:25:43 -0500

    endpoint delete and update

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,26 +1,41 @@
 
 class Api::V1::ProductsController < ApplicationController
-	
+
 	skip_before_filter :verify_authenticity_token  
 
 	def index
 		@products = Product.all
 		render json: @products
 	end
-	
-  def create
-    product = Product.new(product_params)
-    if product.save
-      render  status: 201, json: product
-    else
-      render  status: 422, json: { errors: product.errors } 
-    end
-  end
-  
- 
-  private
-    def product_params
-      params.require(:product).permit(:name, :price)
-    end
-  
+
+	def create
+		product = Product.new(product_params)
+		if product.save
+			render  status: 201, json: product
+		else
+			render  status: 422, json: { errors: product.errors } 
+		end
+	end
+
+	def update
+		product = Product.find(params[:id])
+		if product.update(product_params)
+			render  status: 200, json: product
+		else
+			render  status: 422, json: { errors: product.errors } 
+		end
+	end
+
+	def destroy
+		product = Product.find(params[:id])
+		if product.destroy
+			render status:204
+		end
+	end
+
+	private
+	def product_params
+		params.require(:product).permit(:name, :price)
+	end
+
 end
